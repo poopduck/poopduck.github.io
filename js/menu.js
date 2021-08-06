2425ed0c38a614dbafdfb3be7166b7333094d3eb
poopduck <50244123+poopduck@users.noreply.github.com>
2021-08-06 14:53:35 -0700

    added a responsive nav bar

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+//open menu
+window.onload = function () {
+    const menu_icon = document.querySelector(".menu-icon");
+    const nav_items = document.querySelector(".nav-items");
+
+    menu_icon.onclick = mobileMenu;
+
+    function mobileMenu() {
+        menu_icon.classList.toggle("active");
+        nav_items.classList.toggle("active");
+    }
+}
+
+
+
+//close menu upon clicking a link
+const nav_link = document.querySelectorAll(".nav-link");
+
+nav_link.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    menu_icon.classList.remove("active");
+    nav_items.classList.remove("active");
+}
